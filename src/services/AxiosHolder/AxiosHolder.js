e58b5653b2d3191d286229d128c328b5f740c469
Harsha-lakmal <lakmalharsha424@gmail.com>
2025-04-28 08:33:06 +0530

    develop

--- src/services/AxiosHolder/AxiosHolder.js
+++ src/services/AxiosHolder/AxiosHolder.js
@@ -14,7 +14,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://192.168.171.184:8080/api/v1', 
+  baseURL: 'http://192.168.73.184:8080/api/v1', 
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
